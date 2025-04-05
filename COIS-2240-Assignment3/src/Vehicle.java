33cabd3f51bcdca22e439f7cf232278775520c7e
Scoot3658 <scoot2006scoot@gmail.com>
2025-04-04 23:09:40 -0400

    Task 1 Part 5 Implemented
    Reduced duplication by making a private capitalize function for Vehicle.

--- COIS-2240-Assignment3/src/Vehicle.java
+++ COIS-2240-Assignment3/src/Vehicle.java
@@ -8,16 +8,8 @@ public abstract class Vehicle {
     public enum VehicleStatus { AVAILABLE, RESERVED, RENTED, MAINTENANCE, OUTOFSERVICE }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
+    	this.make = capitalize(make);
+    	this.model = capitalize(model);
         this.year = year;
         this.status = VehicleStatus.AVAILABLE;
         this.licensePlate = null;
@@ -47,6 +39,14 @@ public abstract class Vehicle {
 
     public String getInfo() {
         return "| " + licensePlate + " | " + make + " | " + model + " | " + year + " | " + status + " |";
+    }
+    
+    //Task 1 Part 5
+    private String capitalize(String input) {
+        if (input == null || input.isEmpty()) {
+            return null;
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
 
 }
